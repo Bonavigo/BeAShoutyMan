72113de89e05b3eac1aefb2310473b39b7aad281
Bruno Bonavigo <71879062+Bonavigo@users.noreply.github.com>
2021-12-22 11:12:49 -0300

    HI
    I'M A SHOUTY MAN

---
+++ BeAShoutyMan.py
@@ -0,0 +1,22 @@
+import sys
+from g_python.gextension import Extension
+from g_python.hmessage import Direction, HMessage
+
+extension_info = {
+	"title": "Be a Shouty Man",
+	"description": "A simple extension to shout all the time. And yes, the name is a joke with Horrible Histories. This is just a test extension.",
+	"version": "1.0",
+	"author": "BrunoBonamigo"
+}
+
+ext = Extension(extension_info, sys.argv)
+ext.start()
+
+def forceShout(message):
+	(text, color, index) = message.packet.read('sii')
+	message.is_blocked = True
+	color = str(color)
+	index = str(index)
+	ext.send_to_server('{out:Shout}{s:"'+text+'"}{i:'+color+'}{i:'+index+'}')
+
+ext.intercept(Direction.TO_SERVER, forceShout, 'Chat')
